34a7bbd1a314dcd3130890fbef00b952c4122f3c
S-Gissat <sophiecharlotte.gissat@gmail.com>
2026-08-29 15:11:26 +0200

    flake8

--- module_00/ex2/ft_plot_area.py
+++ module_00/ex2/ft_plot_area.py
@@ -1,2 +1,3 @@
 def ft_plot_area():
-    print("Plot area:", int(input("Enter length: ")) * int(input("Enter width: ")))
+    print("Plot area:", int(input("Enter length: ")) *
+          int(input("Enter width: ")))
